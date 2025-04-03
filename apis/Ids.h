666b2a9fee3e1351780f93ad2f33a8950f9b9a92
mukesh972 <mukesh.m@tataelxsi.co.in>
2025-04-03 15:36:25 +0530

    Update Ids.h
    Add COM-RPC support to FrameRate Plugin

--- apis/Ids.h
+++ apis/Ids.h
@@ -260,7 +260,10 @@ namespace Exchange {
         ID_AMAZONPRIME_NOTIFICATION                  = ID_AMAZONPRIME + 1,
 
         ID_SCREEN_CAPTURE                            = ID_ENTOS_OFFSET + 0x360,
-        ID_SCREEN_CAPTURE_NOTIFICATION               = ID_SCREEN_CAPTURE + 1
+        ID_SCREEN_CAPTURE_NOTIFICATION               = ID_SCREEN_CAPTURE + 1,
+         
+        ID_FRAMERATE                               = ID_ENTOS_OFFSET + 0x370,
+        ID_FRAMERATE_NOTIFICATION                  = ID_FRAMERATE + 1
     };
 }
 }
